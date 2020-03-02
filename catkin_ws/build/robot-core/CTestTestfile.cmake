47339368a97757da844aa935bcf5beec92e6cd2e
Manuel <mlanas@asusx550ld.fritz.box>
2020-03-02 16:57:48 +0100

    creating catkin_ws base structure

---
+++ catkin_ws/build/robot-core/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mlanas/Projects/pass-the-butter/catkin_ws/src/robot-core
+# Build directory: /home/mlanas/Projects/pass-the-butter/catkin_ws/build/robot-core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/unity-sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mlanas/Projects/pass-the-butter/catkin_ws/src/unity-sim
+# Build directory: /home/mlanas/Projects/pass-the-butter/catkin_ws/build/unity-sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
